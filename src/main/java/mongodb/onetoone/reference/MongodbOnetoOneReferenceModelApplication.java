c3c42e4b2e023fcc438cad5e863f06363bbdb0be
Mallesh <u.malleswararao@gmail.com>
2021-07-16 20:04:21 +0530

    Testing WebHook

--- src/main/java/mongodb/onetoone/reference/MongodbOnetoOneReferenceModelApplication.java
+++ src/main/java/mongodb/onetoone/reference/MongodbOnetoOneReferenceModelApplication.java
@@ -8,6 +8,7 @@ public class MongodbOnetoOneReferenceModelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbOnetoOneReferenceModelApplication.class, args);
+		
 	}
 
 }
