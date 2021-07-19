f3a28c19d86e3bc2ecf2ec8b75e04a4028c4d918
Mallesh <u.malleswararao@gmail.com>
2021-07-19 22:11:04 +0530

    Update MongodbOnetoOneReferenceModelApplication.java

--- src/main/java/mongodb/onetoone/reference/MongodbOnetoOneReferenceModelApplication.java
+++ src/main/java/mongodb/onetoone/reference/MongodbOnetoOneReferenceModelApplication.java
@@ -9,5 +9,4 @@ public class MongodbOnetoOneReferenceModelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbOnetoOneReferenceModelApplication.class, args);
 	}
-	
 }
