009fbaf99e941ed521de7ed26710d0c83c903c0c
Mallesh <u.malleswararao@gmail.com>
2021-07-19 11:29:47 +0530

    Testing GitHub webhook

--- src/main/java/mongodb/onetoone/reference/MongodbOnetoOneReferenceModelApplication.java
+++ src/main/java/mongodb/onetoone/reference/MongodbOnetoOneReferenceModelApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongodbOnetoOneReferenceModelApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbOnetoOneReferenceModelApplication.class, args);
 	}
